e1950e88628ca91d13b6753d0e3e67209e401d92
davidemarchini <marchini73@gmail.com>
2024-02-09 18:34:02 +0100

    array di foto

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,138 @@
-<script setup></script>
+<script>
+import AppCard from "./AppCard.vue";
+
+export default {
+  data() {
+    return {
+      title: "Boolando",
+      products: [
+        {
+          id: 1,
+          frontImage: "1.webp",
+          backImage: "1b.webp",
+          brand: "Levi's",
+          name: "Relaxed Fit",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 2,
+          frontImage: "2.webp",
+          backImage: "2b.webp",
+          brand: "Guess",
+          name: "Roses Tee",
+          price: 20.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 3,
+          frontImage: "3.webp",
+          backImage: "3b.webp",
+          brand: "Come Zucchero Filato",
+          name: "Voglia di colori pastello",
+          price: 129.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 4,
+          frontImage: "4.webp",
+          backImage: "4b.webp",
+          brand: "Levi's",
+          name: "Tee Unisex",
+          price: 14.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 5,
+          frontImage: "5.webp",
+          backImage: "5b.webp",
+          brand: "Maya Deluxe",
+          name: "Stripe Bodice",
+          price: 99.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 6,
+          frontImage: "6.webp",
+          backImage: "6b.webp",
+          brand: "Esprit",
+          name: "Maglione - Black",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  components: { AppCard },
+};
+</script>
 
 <template>
+  <!-- ### header  -->
   <div class="header">
-    <div>Donna Uomo Bambino</div>
-    <div><img class="logo" src="../img/boolean-logo.png" alt="" /></div>
-    <div>right</div>
+    <div class="item-header">Donna Uomo Bambino</div>
+    <div class="item-header">
+      <img class="logo" src="./assets/img/boolean-logo.png" alt="" />
+    </div>
+    <div class="item-header">right</div>
   </div>
-  <div>main</div>
+  <!-- ### main  -->
+  <AppCard
+    v-for="product in products"
+    :img="'./img/' + product.frontImage"
+    :title="product.name + ' ciao'"
+  />
+
+  <!-- ### footer  -->
   <div>footer</div>
 </template>
 
---
+++ src/AppCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    img: String,
+    title: String,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="img" :alt="title" />
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  position: relative;
+  width: calc((100% / 3) - 20px);
+  min-width: 300px;
+  border: 1px dashed black;
+  margin: 10px;
+  min-height: 200px;
+}
+</style>
